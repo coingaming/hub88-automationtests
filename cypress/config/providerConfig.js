42d94cfdb907458e16b49855958b915a6b4e9042
Bruno Peres <bruno.peres@yolo.com>
2025-07-10 17:46:26 +0300

    fixing project structure

--- cypress/config/providerConfig.js
+++
@@ -1,7 +0,0 @@
-export const baseUrl = 'https://staging.the-rgs.com/api'; // Replace with actual Provider API URL https://gamelist-two.vercel.app
-export const signature = 'YOUR_PROVIDER_SIGNATURE';
-export const providerID = 'hub88-caleta';
-export const currencyCode = 'EUR, SSS';
-export const gameCode = 'cg_astrowild, cg_basketballpro';
-export const countryCode = 'FI';
-export const langCode = 'en';
