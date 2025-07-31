762fd2a801c8ebed6acaf87101c56b291f07b5cb
Bruno Peres <bruno.peres@yolo.com>
2025-07-31 16:29:47 +0300

    fix: supplier selection

--- boAutomationPw/pageObjects/transactions.js
+++ boAutomationPw/pageObjects/transactions.js
@@ -4,7 +4,9 @@ module.exports = {
     applyRangeButton: 'button:has-text("Apply")',
     supplierFilterTab: 'xpath=/html/body/div/div/div/main/div/div[1]/div[1]/div[5]/button',
     supplierDropdown: 'xpath=/html/body/div/div/div/main/div/div[1]/div[2]/div/button',
-    supplierSearchInput: 'input[placeholder="Search for a name..."]',
+    supplierBox: 'div[class*="FilterDropdown__FilterDropdownWrapper"]',
+    supplierSearchInput: '//*[@id="root"]/div/div/main/div/div[1]/div[2]/div/div/div/div[1]/div/input',
     firstSupplierButton: 'xpath=/html/body/div/div/div/main/div/div[1]/div[2]/div/div/div/div[3]/div[1]',
-    firstTableRoundDetailsButton: 'xpath=/html/body/div/div/div/main/div/div/3/div[1]/table/tbody/tr[1]/td[10]/div[2]/button[1]'    
+    firstTableRoundDetailsButton: 'xpath=/html/body/div/div/div/main/div/div/3/div[1]/table/tbody/tr[1]/td[10]/div[2]/button[1]',
+    transactionsText: 'div:has-text("Transactions")'
 };
--- boAutomationPw/tests/roundDetails.spec.js
+++ boAutomationPw/tests/roundDetails.spec.js
@@ -6,15 +6,18 @@ const {
   applyRangeButton,
   supplierFilterTab,
   supplierDropdown,
+  supplierBox,
   supplierSearchInput,
   firstSupplierButton,
-  firstTableRoundDetailsButton
+  firstTableRoundDetailsButton,
+  transactionsText
 } = require('../pageObjects/transactions.js');
 
 test.use({ storageState: 'storageState.json' });
 
 const suppliers = [
-  "Pragmatic Play", "Hacksaw Gaming", "Evolution Gaming", "Play'n Go", "Nolimit City", "3Oaks", "PGSoft", "Push Gaming", "Live88", "Amusnet", "OneTouch", "Playson", "NetEnt", "VoltEnt", "Peter & Sons", "Relax Gaming", "Endorphina", "Spribe", "Spinomenal", "Penguin King", "Betsoft", "Novomatic", "Microgaming Live", "BGaming", "Blueprint Gaming", "Games Global", "Pragmatic Play Live", "AvatarUX", "RubyPlay", "Dragon Gaming", "Red Tiger Gaming", "Kalamba Games", "KA Gaming", "TrueLab", "Microgaming", "Caleta Gaming", "ShadyLady", "Ezugi", "Fazi", "Turbogames", "Thunderkick", "SlotMill Games", "SmartSoft", "BetsyGames", "Just Slots", "Barbarabang", "Evoplay Entertainment", "Big Time Gaming", "BELATRA", "Platipus", "GameArt", "Oddin", "Habanero", "Popiplay", "Aviatrix", "Mascot Gaming", "Red Rake Gaming", "Octoplay", "Booming Games", "Gamomat", "Winfinity", "Fugaso", "Gamzix", "Fantasma", "Onlyplay", "155.io", "Gameburger", "SwinttPremium", "Darwin Gaming", "Triple Edge", "Apparat Gaming", "TopSpin Games", "Retro Gaming", "Split The Pot", "Mancala Gaming", "Wazdan", "Creedroomz", "BetGames.TV", "Rogue", "G Games", "1x2gaming", "7777Gaming", "NetGaming", "KingMidas", "Oryx Gaming", "GamingCorps", "ElaGames", "SwinttGames", "Playnetic", "WinFast", "CQ9", "SpinPlay Games", "Tangente", "Atomic Slot Lab", "SpinLogic", "SkyWind Live", "JVL", "Yolted", "7Mojos", "FBastards", "Popok", "Felix Gaming", "Stormcraft", "Neon Valley", "Galaxsys", "VivoGaming", "Northern Lights Gaming", "Snowborn Studios", "AirDice", "Pascal", "SkyWind", "JDB Gaming", "Spinthon", "PlayAce", "Golden Race", "LiveGames", "Alchemy Gaming", "NE Games", "Red Desert", "Popok Live", "Pragmatic Play Virtual Sports", "Flatdog Games", "Live Solutions", "Livespins", "Sexy", "Phoenix 7", "Buck Stakes Entertainment", "Speedy Tomatoes", "GP1", "IGTech", "Famous Games", "H27", "OneGame", "Wild Gaming", "100HP Gaming", "Animo Live", "Brino Games", "Victory Ark", "SKILLEX GAMES", "ColossusBets", "Elbet", "Studio21", "5G", "VegasLounge", "clawbuster", "Konquer", "Gosh", "Boldplay", "Popok Instant", "Gametech", "Slingshot", "Foxium", "Kero", "SlotMatrix", "All41", "Zillion", "Bitblox Games", "Low6", "JFTW", "7Mojos Live"
+  "Pragmatic Play"
+  // "Pragmatic Play", "Hacksaw Gaming", "Evolution Gaming", "Play'n Go", "Nolimit City", "3Oaks", "PGSoft", "Push Gaming", "Live88", "Amusnet", "OneTouch", "Playson", "NetEnt", "VoltEnt", "Peter & Sons", "Relax Gaming", "Endorphina", "Spribe", "Spinomenal", "Penguin King", "Betsoft", "Novomatic", "Microgaming Live", "BGaming", "Blueprint Gaming", "Games Global", "Pragmatic Play Live", "AvatarUX", "RubyPlay", "Dragon Gaming", "Red Tiger Gaming", "Kalamba Games", "KA Gaming", "TrueLab", "Microgaming", "Caleta Gaming", "ShadyLady", "Ezugi", "Fazi", "Turbogames", "Thunderkick", "SlotMill Games", "SmartSoft", "BetsyGames", "Just Slots", "Barbarabang", "Evoplay Entertainment", "Big Time Gaming", "BELATRA", "Platipus", "GameArt", "Oddin", "Habanero", "Popiplay", "Aviatrix", "Mascot Gaming", "Red Rake Gaming", "Octoplay", "Booming Games", "Gamomat", "Winfinity", "Fugaso", "Gamzix", "Fantasma", "Onlyplay", "155.io", "Gameburger", "SwinttPremium", "Darwin Gaming", "Triple Edge", "Apparat Gaming", "TopSpin Games", "Retro Gaming", "Split The Pot", "Mancala Gaming", "Wazdan", "Creedroomz", "BetGames.TV", "Rogue", "G Games", "1x2gaming", "7777Gaming", "NetGaming", "KingMidas", "Oryx Gaming", "GamingCorps", "ElaGames", "SwinttGames", "Playnetic", "WinFast", "CQ9", "SpinPlay Games", "Tangente", "Atomic Slot Lab", "SpinLogic", "SkyWind Live", "JVL", "Yolted", "7Mojos", "FBastards", "Popok", "Felix Gaming", "Stormcraft", "Neon Valley", "Galaxsys", "VivoGaming", "Northern Lights Gaming", "Snowborn Studios", "AirDice", "Pascal", "SkyWind", "JDB Gaming", "Spinthon", "PlayAce", "Golden Race", "LiveGames", "Alchemy Gaming", "NE Games", "Red Desert", "Popok Live", "Pragmatic Play Virtual Sports", "Flatdog Games", "Live Solutions", "Livespins", "Sexy", "Phoenix 7", "Buck Stakes Entertainment", "Speedy Tomatoes", "GP1", "IGTech", "Famous Games", "H27", "OneGame", "Wild Gaming", "100HP Gaming", "Animo Live", "Brino Games", "Victory Ark", "SKILLEX GAMES", "ColossusBets", "Elbet", "Studio21", "5G", "VegasLounge", "clawbuster", "Konquer", "Gosh", "Boldplay", "Popok Instant", "Gametech", "Slingshot", "Foxium", "Kero", "SlotMatrix", "All41", "Zillion", "Bitblox Games", "Low6", "JFTW", "7Mojos Live"
 ];
 
 // Use config instead of hardcoded URL
@@ -37,9 +40,13 @@ test.describe('Supplier roundDetails GraphQL check', () => {
       // Open Supplier Filter
       await page.locator('xpath=/html/body/div/div/div/main/div/div[1]/div[1]/div[5]/div/div/div/div[9]').click();
 
-      // Open supplier dropdown
-      await page.locator(supplierDropdown).click();
+      // Open supplier dropdown only if supplierBox is not visible
+      const isSupplierBoxVisible = await page.locator(supplierBox).isVisible();
+      if (!isSupplierBoxVisible) {
+        await page.locator(supplierDropdown).click();
+      }
 
+      // click the input field to search for the supplier
       const input = await page.waitForSelector(supplierSearchInput, { state: 'visible' });
       await input.click({ clickCount: 3 }); // Select existing text
       await input.fill('');                 // Clear the field
@@ -51,7 +58,7 @@ test.describe('Supplier roundDetails GraphQL check', () => {
       await firstSupplierOption.click();
 
       // Click outside to confirm selection
-      await page.locator('xpath=/html/body/div/div/div/main/div/div[1]/div[2]/div/div/div').click();
+      await page.locator(transactionsText).click();
 
       // Confirm selection by clicking outside
       await page.locator('xpath=/html/body/div/div/div/main/div/div[1]/div[2]/div/div/div').click();
