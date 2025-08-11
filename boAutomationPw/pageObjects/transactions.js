31dd42db84ab806c93c1ccafcc92bf903b554a69
Bruno Peres <bruno.peres@yolo.com>
2025-08-11 14:50:50 +0300

    supplierSearchInput xpath updated to selector

--- boAutomationPw/pageObjects/transactions.js
+++ boAutomationPw/pageObjects/transactions.js
@@ -3,7 +3,7 @@ module.exports = {
     rangeOption: 'li:has-text("Last Week")',
     applyRangeButton: 'button:has-text("Apply")',
     addFilterTab: 'button:has-text("Add Filter")',
-    supplierSearchInput: '//*[@id="root"]/div/div/main/div/div[1]/div[2]/div[1]/div/div/div[1]/div/input',
+    supplierSearchInput: 'input[placeholder="Search for a name..."]',
     firstTableRoundDetailsButton: 'button[aria-label*="Round details"]',
     statusSearchInput: 'button:has-text("Statuses: All") >> xpath=..//div[div/p[normalize-space(.)="Success"]]',
 };
