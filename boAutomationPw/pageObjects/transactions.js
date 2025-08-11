7c2f3e769f6dfb919e3d0a1aa1c9f44d80c11c6d
Bruno Peres <bruno.peres@yolo.com>
2025-08-11 14:38:30 +0300

    supplier tab selection mapped - xpath removed

--- boAutomationPw/pageObjects/transactions.js
+++ boAutomationPw/pageObjects/transactions.js
@@ -4,7 +4,6 @@ module.exports = {
     applyRangeButton: 'button:has-text("Apply")',
     addFilterTab: 'button:has-text("Add Filter")',
     supplierSearchInput: '//*[@id="root"]/div/div/main/div/div[1]/div[2]/div[1]/div/div/div[1]/div/input',
-    firstSupplierButton: 'xpath=/html/body/div/div/div/main/div/div[1]/div[2]/div/div/div/div[3]/div[1]',
     firstTableRoundDetailsButton: 'button[aria-label*="Round details"]',
     statusSearchInput: 'button:has-text("Statuses: All") >> xpath=..//div[div/p[normalize-space(.)="Success"]]',
 };
