2ec717fc68e97da0b4d554cc99d75ce8d2e69ecf
Bruno Peres <bruno.peres@yolo.com>
2025-08-22 17:20:29 +0300

    feat: select country and fill number

--- payments-automation/onboarding-ui/tests/onboard-process.spec.js
+++ payments-automation/onboarding-ui/tests/onboard-process.spec.js
@@ -1,34 +1,51 @@
 const { test, expect } = require('@playwright/test');
-const { 
-  baseURL,
-  userProfileButton,
-  logoutButton,
-  nameInput
- } = require('../pageObjects/selectors.js');
+const selectors = require('../utils/selectors.js');
 
 test.use({ storageState: 'storageState.json' });
 
 test.describe('Onboarding Process Tests', () => {
-  // 
+
   test('Should be able to click user avatar', async ({ page }) => {
     // Navigate to the application
-    await page.goto(baseURL, { timeout: 5000 });
+    await page.goto(selectors.baseURL, { timeout: 5000 });
 
     // Locate the avatar button via XPath
-    await page.locator(userProfileButton).click({ timeout: 3000 });
+    await page.locator(selectors.userProfileButton).click({ timeout: 3000 });
     console.log('✅ User avatar click test passed');
 
     // Locate the logout button via XPath
-    await expect(page.locator(logoutButton)).toBeVisible();
+    await expect(page.locator(selectors.logoutButton)).toBeVisible();
     console.log('✅ Logout button visibility test passed');
   });
 
   test('Should be able to fill organization name', async ({ page }) => {
     // Navigate to the application
-    await page.goto(baseURL, { timeout: 5000 });
+    await page.goto(selectors.baseURL, { timeout: 5000 });
 
-    await page.locator(nameInput).fill('qaTest Organization');
-    await expect(page.locator(nameInput)).toHaveValue('qaTest Organization');
+    await page.locator(selectors.nameInput).fill('qaTest Organization');
+    await expect(page.locator(selectors.nameInput)).toHaveValue('qaTest Organization');
     console.log('✅ Organization name test passed');
+  });
+
+  test('Should be able to select country', async ({ page }) => {
+    // Navigate to the application
+    await page.goto(selectors.baseURL, { timeout: 5000 });
+
+    await expect(page.locator(selectors.countryButton)).toBeVisible();
+    await page.locator(selectors.countryButton).click({ timeout: 3000 });
+    console.log('✅ Country button click test passed');
+
+    await page.locator(selectors.brazilCountry).click({ timeout: 3000 });
+    await expect(page.getByText('+55 🇧🇷')).toBeVisible();
+    console.log('✅ Country code test passed');
+  });
+
+  test('Should be able to fill phone number', async ({ page }) => {
+    // Navigate to the application
+    await page.goto(selectors.baseURL, { timeout: 5000 });
+
+    await page.locator(selectors.phoneInput).fill('123456789');
+    await expect(page.locator(selectors.phoneInput)).toHaveValue('123456789');
+    console.log('✅ Phone number test passed');
   });
 });
--- payments-automation/onboarding-ui/utils/selectors.js
+++ payments-automation/onboarding-ui/utils/selectors.js
@@ -2,9 +2,17 @@ module.exports = {
     baseURL: 'https://hw-merch-console.server4.ih.testenv.io/onboarding',
 
     // data-testid="user-profile-button"
+    // userProfileButton: 'button[data-testid="user-profile-button"]',
     userProfileButton: '//html/body/div/div/div/div/div[2]/div[1]/div[2]/div[2]/div/button/div',
+
     // data-testid="logout-button"
+    // logoutButton: 'button[data-testid="logout-button"]',
     logoutButton: '//html/body/div/div/div/div/div[2]/div[1]/div[2]/div[2]/div/ul/span/button',
 
+    // data-testid not needed
     nameInput: 'input[placeholder="Organization Name"]',
+    phoneInput: 'input[placeholder="Phone Number"]',
+    brazilCountry: '//li[normalize-space()="🇧🇷Brazil (+55)"]',
+    countryButton: '//button[.//p[normalize-space()="Country code"]]',
+    
 };
